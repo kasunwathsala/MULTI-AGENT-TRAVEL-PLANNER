952ba96ff28e18eca3add93cf792c148b9209478
kasunwathsala <wathsala.nilaweera2001@gmail.com>
2026-01-05 01:03:27 +0530

    Fix import statement for ChatOpenAI and ensure proper agent creation with flight availability tool

--- app/agents/requirment_agent.py
+++ app/agents/requirment_agent.py
@@ -1,5 +1,15 @@
 from langchain.agents import create_agent
+from lagnchain_openai import ChatOpenAI
 
 from app.agents.promots import REQUIREMENTS_AGENT_SYSTEM_PROMPT
 from app.agents.response_models import RequirementsAgentResponseModel
-from app.agents.tools.flight_tools import search_flight_availability
+from app.agents.tools import search_flight_availability
+
+llm = ChatOpenAI(model=settings.OPENAI_MODEL_NAME, api=settings.OPENAI_API_KEY)
+
+agent = create_agent(
+    model=llm,
+    tools=[search_flight_availability],
+    system_prompt=REQUIREMENTS_AGENT_SYSTEM_PROMPT,
+    response_model=RequirementsAgentResponseModel,
+)
